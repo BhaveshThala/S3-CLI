9831abc71d9437688101bf3be687c78aff3f1009
BhaveshThala <bhaveshsrivastava50@gmail.com>
2024-02-03 13:20:32 +0530

    s3 tool to download files from aws-s3 based on regex and date

---
+++ Helper/AwsListObjectHelper.cs
@@ -0,0 +1,102 @@
+﻿using System.IO.Compression;
+using System.Text.RegularExpressions;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+
+namespace S3RegexBasedDownloader.Helper;
+
+public class AwsListObjectHelper
+{
+    private readonly string _accessKey;
+    private readonly string _secretKey;
+    private readonly RegionEndpoint _regionEndpoint;
+
+    public AwsListObjectHelper(string accessKey, string secretKey, string regionName)
+    {
+        _accessKey = accessKey;
+        _secretKey = secretKey;
+        _regionEndpoint = RegionEndpoint.GetBySystemName(regionName);
+    }
+    public async Task<MemoryStream> ListObjects(string bucketName, string? prefix,string? regexPattern,string searchCondition)
+    {
+        Dictionary<string,MemoryStream> allRequestedFiles = new Dictionary<string, MemoryStream>();
+        using var client = new AmazonS3Client(_accessKey, _secretKey, _regionEndpoint);
+        ListObjectsV2Request  listRequest = string.IsNullOrEmpty(prefix)
+            ? new ListObjectsV2Request 
+            {
+                BucketName = bucketName,
+            }
+            : new ListObjectsV2Request ()
+            {
+                BucketName = bucketName,
+                Prefix = prefix,
+                StartAfter = prefix,
+            };
+
+        ListObjectsV2Response  listResponse;
+            
+        do
+        {
+            listResponse = client.ListObjectsV2Async(listRequest).GetAwaiter().GetResult();
+            foreach (S3Object obj in listResponse.S3Objects)
+            {
+                string fileName = Path.GetFileName(obj.Key);
+
+                if (searchCondition.ToLower() == "yes")
+                {
+                    var dateData = DateTime.Parse(regexPattern!);
+                    if (obj.LastModified.Date >= dateData.Date)
+                    {
+                        // Download the object
+                        var memoryStream = await DownloadObjectAsync(client, bucketName, obj.Key);
+                        allRequestedFiles[fileName] = memoryStream;
+                    }
+                }
+                else
+                {
+                    if (Regex.IsMatch(fileName, regexPattern!))
+                    {
+                        // Download the object
+                        var memoryStream = await DownloadObjectAsync(client, bucketName, obj.Key);
+                        allRequestedFiles[fileName] = memoryStream;
+                    }
+                }
+            }
+                
+            listRequest.ContinuationToken = listResponse.NextContinuationToken;
+        } while (listResponse.IsTruncated);
+
+        return CreateZipFile(allRequestedFiles);
+    }
+    private async Task<MemoryStream> DownloadObjectAsync(AmazonS3Client s3Client, string bucketName, string objectKey)
+    {
+        TransferUtility fileTransferUtility = new TransferUtility(s3Client,new TransferUtilityConfig()
+        {
+            ConcurrentServiceRequests = 10,
+        });
+        Stream stream = await fileTransferUtility.OpenStreamAsync(bucketName, objectKey);
+        MemoryStream memoryStream = new MemoryStream();
+        await stream.CopyToAsync(memoryStream);
+        
+        memoryStream.Seek(0, SeekOrigin.Begin);
+        return memoryStream;
+    }
+    private MemoryStream CreateZipFile(Dictionary<string, MemoryStream> nestedFileNames)
+    {
+        MemoryStream compressedFileStream = new MemoryStream();
+
+        using var zipArchive = new ZipArchive(compressedFileStream, ZipArchiveMode.Create, true);
+
+        foreach (var item in nestedFileNames)
+        {
+            ZipArchiveEntry zipEntry = zipArchive.CreateEntry(item.Key);
+
+            using var zipEntryStream = zipEntry.Open();
+            item.Value.Seek(0, SeekOrigin.Begin);
+            item.Value.CopyTo(zipEntryStream);
+        }
+        return compressedFileStream;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using S3RegexBasedDownloader.Helper;
+
+//access key = AKIAVHUTVHFSUVH2NAWC
+//secret key = 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
+// region = ap-south-2
+// bucket name = prescreening-storage-v2-bucket
+// folder name = Prescreening/PrescreeningStaging/
+// regex pattern = .*.20240118.*.txt
+// file output path = D:\LogsData\
+
+Start:
+Console.WriteLine("Enter Access Key: ");
+string? accessKey = Console.ReadLine()!.Trim();
+
+Console.WriteLine("Enter Secret Key: ");
+string? secretKey = Console.ReadLine()!.Trim();
+
+Console.WriteLine("Enter Aws Region: ");
+string? region = Console.ReadLine()!.Trim();
+
+Console.WriteLine("Enter bucket name: ");
+string? bucketName = Console.ReadLine()!.Trim();
+
+Console.WriteLine("Enter Folder Name: ");
+string? folderName = Console.ReadLine()!.Trim();
+
+Console.WriteLine("Enter Regex Pattern or Add Date Format of Date is: yyyy-mm-dd");
+string? regexPattern = Console.ReadLine()!.Trim();
+
+Console.WriteLine("Enter Search Condition yes for Date Search and No for Regex based search");
+string? searchCondition = Console.ReadLine()!.Trim();
+
+Console.WriteLine("Enter File Output Path: ");
+string? filePath = Console.ReadLine()!.Trim();
+
+if (searchCondition.ToLower().Trim() == "yes")
+{
+    try
+    {
+        var temp = DateTime.Parse(regexPattern!);
+    }
+    catch
+    {
+        Console.WriteLine("Invalid Date format");
+        Console.WriteLine("Enter correct Date format of Date is: yyyy-mm-dd");
+        regexPattern = Console.ReadLine()!.Trim();
+    }
+}
+
+
+
+if(string.IsNullOrEmpty(accessKey) || string.IsNullOrEmpty(secretKey) || string.IsNullOrEmpty(region) || string.IsNullOrEmpty(bucketName) || string.IsNullOrEmpty(regexPattern) || string.IsNullOrEmpty(filePath)
+   || string.IsNullOrEmpty(searchCondition))
+{
+    Console.WriteLine("Required Field : Access Key");
+    Console.WriteLine("Required Field : Secret Key");
+    Console.WriteLine("Required Field : Region");
+    Console.WriteLine("Required Field : Bucket Name");
+    Console.WriteLine("Required Field : Regex Pattern or Date");
+    Console.WriteLine("Required Field : File Output Path");
+    Console.WriteLine("Required Field : Search Condition");
+    goto Start;
+}
+
+Console.WriteLine("Please wait while we are generating your file...");
+
+AwsListObjectHelper listObjectHelper = new AwsListObjectHelper(accessKey!,secretKey!,region!);
+
+MemoryStream? zipFileStream = null;
+
+try
+{
+    zipFileStream = listObjectHelper.ListObjects(bucketName!, folderName, regexPattern,searchCondition).Result;
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    goto Start;
+}
+
+string fileName = @$"{filePath}{new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds()}.zip";
+
+using (FileStream fileStream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+{
+    zipFileStream.WriteTo(fileStream);
+}
+
+Console.WriteLine("File Generated Successfully");
